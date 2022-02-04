4c3a289d1321b8289d29a7ebf08b308651332972
James-L-Jr <james.f.logan2@gmail.com>
2022-02-03 20:55:24 -0600

    Adding react

---
+++ src/main/resources/static/precache-manifest.041dbdc864aad7d723a86521dc4070f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79c65b5d54186eb2e0bf87dc87557f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7260d1ecf2f9bbcdd878",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "88defb7baaa87f9442e9",
+    "url": "/static/css/main.c59fff76.chunk.css"
+  },
+  {
+    "revision": "7260d1ecf2f9bbcdd878",
+    "url": "/static/js/2.4ab6461d.chunk.js"
+  },
+  {
+    "revision": "dac7221200a2440de73372b0e56ca4ba",
+    "url": "/static/js/2.4ab6461d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88defb7baaa87f9442e9",
+    "url": "/static/js/main.e23711e8.chunk.js"
+  },
+  {
+    "revision": "d9dd64ad6b0b94202ef5",
+    "url": "/static/js/runtime-main.2e269df6.js"
+  }
+]);
